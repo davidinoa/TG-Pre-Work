c65bd1a65b9a0817c68ec807e32b468eb9fb94e8
David Inoa <davidinoa@gmail.com>
2019-02-04 19:26:28 -0400

    complete Properties

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Properties.js
@@ -0,0 +1,3 @@
+console.log('Teaching the world how to code'.length);
+
+// Every string instance has a property called length that stores the number of characters in that string. You can retrieve property information by appending the string with a period and the property name.
