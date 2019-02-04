a5d77d04353d7cffaf550fb51e57a27d4c510347
David Inoa <davidinoa@gmail.com>
2019-02-04 19:28:28 -0400

    complete Methods

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Methods.js
@@ -0,0 +1,7 @@
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+// Methods are actions we can perform. JavaScript provides a number of string methods. We call, or use, these methods by appending an instance with a period (the dot operator), the name of the method, and opening and closing parentheses
