4822bb6fc894571c7998d69c324221e4fd8a1f19
David Inoa <davidinoa@gmail.com>
2019-02-04 19:23:27 -0400

    complete Arithmetic-Operators

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Arithmetic-Operators.js
@@ -0,0 +1,6 @@
+console.log(3.5 + 27);
+console.log(2019 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
+
+// An operator is a character that performs a task in our code. JavaScript has several built-in in arithmetic operators, that allow us to perform mathematical calculations on numbers
