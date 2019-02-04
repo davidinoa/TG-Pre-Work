66a473fb390cac5db38efdc5c79b4c5fbd690f6b
David Inoa <davidinoa@gmail.com>
2019-02-04 19:21:30 -0400

    complete Data Types

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Data-Types.js
@@ -0,0 +1,6 @@
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+// Data types are the classifications we give to the different kinds of data that we use in programming. In JavaScript, there are seven fundamental data types: number, string, boolean, null, undefined, symbol, and object.
