cc656db4cc37458b35faa42df11b6ecc7a40b4de
David Inoa <davidinoa@gmail.com>
2019-02-05 14:53:27 -0400

    complete Comparison-Operators

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Comparison-Operators.js
@@ -0,0 +1,9 @@
+let hungerLevel = 7;
+
+if (hungerLevel > 7) {
+  console.log('Time to eat!');
+} else {
+  console.log('We can eat later!');
+}
+
+// When writing conditional statements, sometimes we need to use different types of operators to compare values. These operators are called comparison operators.
