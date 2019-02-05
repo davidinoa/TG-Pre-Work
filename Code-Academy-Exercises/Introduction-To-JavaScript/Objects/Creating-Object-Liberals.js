8a742fa415548cccdfbc8340a9e096124ddcc4b2
David Inoa <davidinoa@gmail.com>
2019-02-05 19:21:47 -0400

    complete Creating-Object-Literals

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Creating-Object-Liberals.js
@@ -0,0 +1,7 @@
+// Write your fasterShip object literal below
+const fasterShip = {
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+};
+
+// We use curly braces, {}, to designate an object literal.
