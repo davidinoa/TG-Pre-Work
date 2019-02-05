400701146c476b3e680b6b62ead25c499e7e667c
David Inoa <davidinoa@gmail.com>
2019-02-05 17:38:06 -0400

    complete The-For-Loop

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/The-For-Loop.js
@@ -0,0 +1,5 @@
+for (let i = 5; i <= 10; i++) {
+  console.log(i);
+}
+
+// Instead of writing out the same code over and over, loops allow us to tell computers to repeat a given block of code on its own.
