8d2b0f2d5145552731bb169c6ef46efc71d16667
David Inoa <davidinoa@gmail.com>
2019-02-05 16:41:33 -0400

    complete Scope

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Scope.js
@@ -0,0 +1,1 @@
+// Scope defines where variables can be accessed or referenced. While some variables can be accessed from anywhere within a program, other variables may only be available in a specific context.
