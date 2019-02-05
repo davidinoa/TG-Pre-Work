b66b317a4978b4f52fbfd6696ca8cebb681e6b92
David Inoa <davidinoa@gmail.com>
2019-02-04 20:13:02 -0400

    complete Create-a-Variable:var

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:var.js
@@ -0,0 +1,6 @@
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+// var, short for variable, is a JavaScript keyword that creates, or declares, a new variable.
