43fadbed1169eae8e2b4bda597a42b9b93f78521
David Inoa <davidinoa@gmail.com>
2019-02-05 18:42:09 -0400

    complete The-forEach-Method

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-forEach-Method.js
@@ -0,0 +1,5 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+fruits.forEach((fruit) => console.log(`I want to eat a ${fruit}`));
+
+// .forEach() will execute the same code for each element of an array.
