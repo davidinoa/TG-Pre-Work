48b709dc35fb62b36431706fc41b318b1ddd4f7b
David Inoa <davidinoa@gmail.com>
2019-02-05 15:29:34 -0400

    complete Function-Declaration

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Function-Declaration.js
@@ -0,0 +1,9 @@
+function getReminder() {
+  console.log('Water the plants.');
+}
+
+function greetInSpanish() {
+  console.log('Buenas Tardes.');
+}
+
+// Just like how a variable declaration binds a value to a variable name, a function declaration binds a function to a name, or an identifier.
