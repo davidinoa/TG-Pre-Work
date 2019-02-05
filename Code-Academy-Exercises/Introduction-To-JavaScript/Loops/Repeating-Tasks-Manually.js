884b0d5141dddbdfdf6992f4a1d99751d89b5c88
David Inoa <davidinoa@gmail.com>
2019-02-05 17:29:24 -0400

    complete Repeating-Tasks-Manually

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,6 @@
+const vacationSpots = ['Barcelona', 'Denver', 'London'];
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+// Loops allow you to avoid doing repeating-tasks manually.
