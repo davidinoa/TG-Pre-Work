45b9c1e8cae3158bed9a05579b155e4954363090
David Inoa <davidinoa@gmail.com>
2019-02-05 19:31:33 -0400

    complete Property-Assignment

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Property-Assignment.js
@@ -0,0 +1,13 @@
+let spaceship = {
+  'Fuel Type': 'Turbo Fuel',
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Secret Mission': 'Discover life outside of Earth.',
+};
+
+// Write your code below
+spaceship.color = 'glorious gold ';
+spaceship.numEngines = 10;
+delete spaceship['Secret Mission'];
+
+// We can use either dot notation or bracket notation, and the assignment operator to add new key-value pairs to an object or change an existing property.
