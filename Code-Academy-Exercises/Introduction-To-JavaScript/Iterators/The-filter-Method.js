4623ff57ae21111b8d84d46236091093db99ceca
David Inoa <davidinoa@gmail.com>
2019-02-05 18:53:01 -0400

    complete The-filter-Method

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-filter-Method.js
@@ -0,0 +1,17 @@
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter((number) => number < 250);
+
+const favoriteWords = [
+  'nostalgia',
+  'hyperbole',
+  'fervent',
+  'esoteric',
+  'serene',
+];
+
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter((word) => word.length > 7);
+
+// .filter() returns an array of elements after filtering out certain elements from the original array.
