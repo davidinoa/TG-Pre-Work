2916652a4945bf8354f413879a7e95033c95b4ec
David Inoa <davidinoa@gmail.com>
2019-02-05 17:46:59 -0400

    complete Looping-through-Arrays

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Looping-through-Arrays.js
@@ -0,0 +1,7 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log(`I would love to visit ${vacationSpots[i]}`);
+}
+
+// for loops are very handy for iterating over data structures such as arrays.
