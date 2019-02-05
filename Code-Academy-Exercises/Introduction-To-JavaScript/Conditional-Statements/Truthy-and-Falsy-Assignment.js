d791b25c860f166fa0f8596f8407069de42bd518
David Inoa <davidinoa@gmail.com>
2019-02-05 15:11:48 -0400

    complete Truthy-and-Falsy-Assignment

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,9 @@
+let tool = '';
+tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+// In a boolean condition, JavaScript assigns the truthy value to a variable if you use the || operator in your assignment
