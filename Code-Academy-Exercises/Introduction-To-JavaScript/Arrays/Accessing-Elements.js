86b15d04a453ddecec005d2659b0fcb3cb5e3b39
David Inoa <davidinoa@gmail.com>
2019-02-05 17:09:00 -0400

    complete Accesing-Elements

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Accessing-Elements.js
@@ -0,0 +1,10 @@
+const famousSayings = [
+  'Fortune favors the brave.',
+  'A joke is a very serious thing.',
+  'Where there is love there is life.',
+];
+
+const listItem = famousSayings[0];
+console.log(famousSayings[3]);
+
+// Each element in an array has a numbered position known as its index. We can access individual items using their index, which is similar to referencing an item in a list based on the item's position.
