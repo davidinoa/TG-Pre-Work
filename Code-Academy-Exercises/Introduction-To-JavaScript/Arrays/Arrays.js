1063cb13fafb72e33d7d673472a0997668af5d25
David Inoa <davidinoa@gmail.com>
2019-02-05 17:04:38 -0400

    complete Arrays

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays.js
@@ -0,0 +1,1 @@
+// Arrays are JavaScript's way of making lists. Arrays can store any data types (including strings, numbers, and booleans). Like lists, arrays are ordered, meaning each item has a numbered position.
