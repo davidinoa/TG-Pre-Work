a32fe42f2054f266e2ccb12c3b1b7ddda95247d9
David Inoa <davidinoa@gmail.com>
2019-02-05 15:14:01 -0400

    complete Ternary-Operator

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Ternary-Operator.js
@@ -0,0 +1,17 @@
+let isLocked = false;
+
+isLocked
+  ? console.log('You will need a key to open the door.')
+  : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!'
+  ? console.log('I love that!')
+  : console.log("I don't love that!");
+
+// Like if...else statements, ternary operators can be used for conditions which evaluate to true or false.
