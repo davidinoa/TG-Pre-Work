c9c43076e675cf775b40620439f6f7342a0f1a01
David Inoa <davidinoa@gmail.com>
2019-02-05 15:33:53 -0400

    complete Parameters-and-Arguments

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Parameters-and-Arguments.js
@@ -0,0 +1,9 @@
+function sayThanks(name) {
+  console.log(
+    'Thank you for your purchase ' + name + '! We appreciate your business.',
+  );
+}
+
+sayThanks('Cole');
+
+// We use parameters as placeholders for information that will be passed to the function when it is called.
