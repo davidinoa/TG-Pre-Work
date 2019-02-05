6da6f5a721186d658dc0121f2ae66a81f7933869
David Inoa <davidinoa@gmail.com>
2019-02-05 19:29:27 -0400

    complete Bracket-Notation

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Bracket-Notation.js
@@ -0,0 +1,14 @@
+let spaceship = {
+  'Fuel Type': 'Turbo Fuel',
+  'Active Mission': true,
+  homePlanet: 'Earth',
+  numCrew: 5,
+};
+
+let propName = 'Active Mission';
+
+// Write your code below
+const isActive = spaceship['Active Mission'];
+console.log(isActive);
+
+// The second way to access a key's value is by using bracket notation, [ ].
