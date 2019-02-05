b6278468e2b74da6878fda16633d77fc28dda9ed
David Inoa <davidinoa@gmail.com>
2019-02-05 19:27:13 -0400

    complete Accessing-Properties

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Accessing-Properties.js
@@ -0,0 +1,13 @@
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn'],
+};
+
+// Write your code below
+const crewCount = spaceship.numCrew;
+const planetArray = spaceship.flightPath;
+
+// To access a property with dot notation, we write the object's name, followed by the dot operator and then the property name.
