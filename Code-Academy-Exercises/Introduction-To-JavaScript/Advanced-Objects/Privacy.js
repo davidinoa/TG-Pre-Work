5822cd71bca53193f6d290055ce58bb6665fd4bc
David Inoa <davidinoa@gmail.com>
2019-02-06 15:17:56 -0400

    complete Privacy

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Privacy.js
@@ -0,0 +1,12 @@
+const robot = {
+  _energyLevel: 100,
+  recharge() {
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`);
+  },
+};
+
+robot._energyLevel = 'high';
+robot.recharge();
+
+// One common convention is to place an underscore _ before the name of a property to mean that the property should not be altered.
