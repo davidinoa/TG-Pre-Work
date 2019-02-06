c1f9f0c0139d5286db0e7abe14e210ea368d1f48
David Inoa <davidinoa@gmail.com>
2019-02-06 16:38:30 -0400

    complete Why-ES6

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Why-ES6.js
@@ -0,0 +1,18 @@
+var pasta = 'Spaghetti'; // ES5 syntax
+
+var meat = 'Pancetta'; // ES6 syntax
+
+var sauce = 'Eggs and cheese'; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara =
+  'You can make carbonara with ' +
+  pasta +
+  ', ' +
+  meat +
+  ', ' +
+  ' and a sauce made with ' +
+  sauce +
+  '.';
+
+// To limit the impact of ES6 browser compatibility issues, Ecma made the new syntax backwards compatible, which means you can map JavaScript ES6 code to ES5.
