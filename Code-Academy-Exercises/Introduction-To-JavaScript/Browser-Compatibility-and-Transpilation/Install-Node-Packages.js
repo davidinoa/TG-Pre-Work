d6c0ec70d0a39ad19a70d106c77f27978dc0f138
David Inoa <davidinoa@gmail.com>
2019-02-06 16:43:18 -0400

    complete Install-Node-Packages

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Install-Node-Packages.js
@@ -0,0 +1,10 @@
+var pasta = 'Spaghetti'; // ES5 syntax
+
+const meat = 'Pancetta'; // ES6 syntax
+
+let sauce = 'Eggs and cheese'; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+// We use the npm install command to install new Node packages locally. The install command creates a folder called node_modules and copies the package files to it. The install command also installs all of the dependencies for the given package.
