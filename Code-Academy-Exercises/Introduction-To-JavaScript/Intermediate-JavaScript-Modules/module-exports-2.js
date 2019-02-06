6736153f18197dd4e485538dddb60ab93c7a3ff5
David Inoa <davidinoa@gmail.com>
2019-02-06 17:12:05 -0400

    complete module-exports-2

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/module-exports-2.js
@@ -0,0 +1,9 @@
+const Airplane = require('./1-airplane.js');
+
+function displayAirplane() {
+  console.log(Airplane.myAirplane);
+}
+
+displayAirplane();
+
+// To make use of the exported module and the behavior we define within it, we import the module. A common way to do this is with the require() function.
