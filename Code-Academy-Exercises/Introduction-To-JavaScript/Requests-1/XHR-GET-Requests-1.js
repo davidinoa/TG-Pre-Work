32dc153142bd17ae532b324181536012590a5081
David Inoa <davidinoa@gmail.com>
2019-02-08 14:33:55 -0400

    complete XHR-GET-Requests-1

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-1/XHR-GET-Requests-1.js
@@ -0,0 +1,1 @@
+// Asynchronous JavaScript and XML (AJAX), enables requests to be made after the initial page load. Initially, AJAX was used only for XML formatted data, now it can be used to make requests that have many different formats.
