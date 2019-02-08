462b7f9bf2c7e9a51cbcb2588302b4bf74395fbc
David Inoa <davidinoa@gmail.com>
2019-02-08 14:06:37 -0400

    complete export-default

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/export-default.js
@@ -0,0 +1,15 @@
+let Airplane = {};
+
+Airplane.availableAirplanes = [
+{
+  name: 'AeroJet',
+  fuelCapacity: 800
+ }, 
+ {name: 'SkyJet',
+  fuelCapacity: 500
+ }
+];
+
+export default Airplane;
+
+// The default export syntax works similarly to the module.exports syntax, allowing us to export one module per file.
