9ba6626ad3ee8233ad9f78a4758e63e4a1700b29
David Inoa <davidinoa@gmail.com>
2019-02-08 15:25:56 -0400

    complete fetch-POST-Requests-1

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-POST-Requests-1.js
@@ -0,0 +1,1 @@
+// The initial POST call takes two arguments: an endpoint and an object that contains information needed for the POST request. The rest of the request is identical to the GET request.
